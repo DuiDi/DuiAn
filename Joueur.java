b52f32e45627b47bcce7f5df786d5db84b22c39a
DydyPhi <andy.angerville@gmail.com>
2016-09-10 12:47:16 +0200

    Motifs AppliDuiAn
    Modifs fonctions AfficheRésultat;
    Suppression boutonQuestionSuivante
    Intégration fonction GetQuiz dans BoutonValider

---
+++ Joueur.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duian;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author DydyBook
+ */
+public class Joueur {
+    private String nom;
+    private int nbTotemsValidés = 0;
+    private int nbVies = 3;
+    private Vector<Totem> TotemsValidés = new Vector<Totem>();
+    
+    @Override
+    public String toString(){
+        String s = "Joueur : "+this.nom+"\nVies restants : "+this.nbVies+"\nTotems obtenus : \n";
+        for(Totem t: this.TotemsValidés){
+            s += t.toString();
+        }
+        return s;
+    }
+    
+    //Permet d'obtenir le nom du joueur
+    public String GetNom(){
+        return this.nom;
+    }
+    
+    //Permet d'obtenir le nombre de totems obtenus
+    public int GetTotemsValidés(){
+        return this.nbTotemsValidés;
+    }
+    
+    //Permet d'obtenir le nombre de vies
+    public int GetNbVies(){
+        return this.nbVies;
+    }
+    
+    //Permet de modifier le nom du joueur
+    public void SetNom(String s){
+        this.nom = s;
+    }
+    
+    //Permet de modifier le nombre de totems obtenus
+    public void SetTotemsValidés(int i){
+        this.nbTotemsValidés = i;
+    }
+    
+    //Permet de modifier le nombre de vie
+    public void SetPlusVie(){
+        this.nbVies++;
+    }
+    
+    //Permet de remettre à zéro les informations du joueur
+    public void Reset(){
+        this.nbTotemsValidés = 0;
+        this.nbVies = 3;
+    }
+    
+    //Permet d'ajouter un totem au vector
+    public void AjoutTotem(Totem t){
+        this.TotemsValidés.add(t);
+    }
+}
